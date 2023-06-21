932f9c54946dd4bd42aae8fe23ef096cdcc1537c
Rômulo Costa <romulopinheirocosta@gmail.com>
2023-06-21 13:13:10 -0300

    validate if reposiotory exists before submiting

--- assets/js/containers/RepoCreateContainer.js
+++ assets/js/containers/RepoCreateContainer.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {createRepository, getRepository} from '../api/CommitAPI';
 import Form from '../components/RepoCreateForm';
+import {connect} from 'react-redux';
+import {
+  createRepository, 
+  getRepository
+} from '../api/CommitAPI';
+import {
+  getRepositorySuccess, 
+  createRepositoryFailure, 
+  createRepositorySuccess
+} from '../actions/RepositoryActions';
 
 class RepoCreateContainer extends React.Component {
   submit = (values, dispatch) => {
     const token = document.getElementById('main').dataset.csrftoken;
     const name = values.name.split('/')[1];
     const v = {...values, name};
-    return createRepository(v, {'X-CSRFToken': token}, dispatch)
-      .then(getRepository());
+
+    const {repositories} = this.props;
+    const repositoryExists = repositories.some(object => object.name === name);
+    if (!repositoryExists) {
+      return createRepository(v, {'X-CSRFToken': token}, dispatch)
+        .then(getRepository(name));
+    }
+
+    dispatch(createRepositoryFailure(null, true));
+    dispatch(createRepositorySuccess(null, false));
   };
 
   render() {
@@ -28,11 +44,13 @@ class RepoCreateContainer extends React.Component {
 RepoCreateContainer.propTypes = {
   successMessage: PropTypes.bool.isRequired,
   errorMessage: PropTypes.bool.isRequired,
+  repositories: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-const mapStateToProps = store => ({
-  successMessage: store.repositoryState.successMessage,
-  errorMessage: store.repositoryState.errorMessage,
+const mapStateToProps = state => ({
+  successMessage: state.repositoryState.successMessage,
+  errorMessage: state.repositoryState.errorMessage,
+  repositories: state.repositoryState.repositories,
 });
 
 export default connect(mapStateToProps)(RepoCreateContainer);
